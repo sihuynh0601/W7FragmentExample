bab9d36527332f46c077d909492a9ec5cdfebd5e
sihuynh0601 <sihuynh0601@github.com>
2017-10-22 10:32:08 +0700

    Initial commit

---
+++ app/src/main/java/com/sihuynh/w7fragmentexample/MainFragment.java
@@ -0,0 +1,20 @@
+package com.sihuynh.w7fragmentexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 22/10/2017.
+ */
+
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main,container,false);
+    }
+}
